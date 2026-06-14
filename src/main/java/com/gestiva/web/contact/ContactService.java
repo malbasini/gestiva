ede78d26ff299168726f5820c524b9bdf4b05ad9
malbasini <malbasini@gmail.com>
2026-06-15 00:02:03 +0200

    IMPLEMENTS HELP WITH OPENAI

--- src/main/java/com/gestiva/web/contact/ContactService.java
+++ src/main/java/com/gestiva/web/contact/ContactService.java
@@ -13,27 +13,25 @@ public class ContactService {
     @Value("${app1.contact.to}")
     private String contactTo;
 
+    @Value("${support.mail.username}")
+    private String fromAddress;
+
     public ContactService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
     public void send(ContactForm form) {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(form.getEmail());
-            message.setTo(contactTo);
-            message.setReplyTo(form.getEmail());
-            message.setSubject("[Gestiva] Richiesta contatto - " + form.getSubject());
-            message.setText(
-                    "Nome: " + form.getName() + "\n" +
-                            "Email: " + form.getEmail() + "\n\n" +
-                            "Messaggio:\n" + form.getMessage()
-            );
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromAddress);
+        message.setTo(contactTo);
+        message.setReplyTo(form.getEmail());
+        message.setSubject("[Gestiva] Richiesta contatto - " + form.getSubject());
+        message.setText(
+                "Nome: " + form.getName() + "\n" +
+                        "Email: " + form.getEmail() + "\n\n" +
+                        "Messaggio:\n" + form.getMessage()
+        );
 
-            mailSender.send(message);
-        }
-        catch (Exception ex) {
-            throw new RuntimeException("Errore durante l'invio del messaggio", ex);
-        }
+        mailSender.send(message);
     }
 }
